ccd70f911b65721f80ef20286b465c3adf951c92
HyunJung <crispylemon7120@gmail.com>
2022-03-25 16:38:33 +0900

    03-25

---
+++ nomadCoders/dist/index.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// //dosen't compile back to javascript
+// interface Human{
+//   name:string;
+//   age:number;
+//   gender:string;
+// }
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const lynn = new Human("Lynn", 54, "female");
+const person = {
+    name: "jenny",
+    age: 24,
+    gender: "female"
+};
+// const sayHi = (name:string, age:number, gender?:string):string => {
+//   return `hello ${name}, ${age}, you are a ${gender}`
+// }
+const sayHi = (person) => {
+    return `hello ${person.name}, ${person.age}, you are a ${person.gender}`;
+};
+console.log(sayHi(lynn));
+//# sourceMappingURL=index.js.map
---
+++ nomadCoders/src/index.ts
@@ -0,0 +1,36 @@
+// //dosen't compile back to javascript
+// interface Human{
+//   name:string;
+//   age:number;
+//   gender:string;
+// }
+class Human{
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name:string, age:number, gender?:string){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+const lynn = new Human("Lynn", 54, "female");
+
+const person = {
+  name: "jenny",
+  age: 24,
+  gender: "female"
+}
+
+// const sayHi = (name:string, age:number, gender?:string):string => {
+//   return `hello ${name}, ${age}, you are a ${gender}`
+// }
+
+const sayHi = (person: Human):string => {
+  return `hello ${person.name}, ${person.age}, you are a ${person.gender}`;
+}
+console.log(sayHi(lynn));
+
+export{};
+  
